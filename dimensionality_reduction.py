8d36273b7940a943c2f9392f79e95659e103fb18
Odin Hoff Gardaa <odinhg@gmail.com>
2022-10-06 15:01:21 +0200

    added umap dimensionality reduction with plot

--- dimensionality_reduction.py
+++ dimensionality_reduction.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from os.path import isfile
 import umap.umap_ as umap
 from sklearn.preprocessing import StandardScaler
-#from configfile import *
+from configfile import class_names_all 
 
 def sample_df(df, n=100):
     if n > df.shape[0]:
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     df_unseen = pd.read_pickle("embeddings_unseen.pkl")
     
     # Randomly subsample images
-    number_of_samples = 1000
+    number_of_samples = 100 #1000
     df_test = sample_df(df_test, number_of_samples)
     df_train = sample_df(df_train, number_of_samples)
     df_unseen = sample_df(df_unseen, number_of_samples)
@@ -41,8 +41,19 @@ if __name__ == "__main__":
     df_projection_train = reducer.transform(df_train.iloc[:,1:])
     df_projection_unseen = reducer.transform(df_unseen.iloc[:,1:])
 
-    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(16, 8))
-    ax[0].scatter(df_projection_test[:,0], df_projection_test[:,1], c=df_test.label_idx, s=5)
+    # Generate and save plot
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
+    s = ax[0].scatter(df_projection_test[:,0], df_projection_test[:,1], c=df_test.label_idx, s=5)
     ax[0].set_aspect("equal", "datalim")
     ax[0].set_title("UMAP (embedded test data)")
-    plt.show()
+    ax[0].legend(handles=s.legend_elements()[0], labels=class_names_all)
+    s = ax[1].scatter(df_projection_train[:,0], df_projection_train[:,1], c=df_train.label_idx, s=5)
+    ax[1].set_aspect("equal", "datalim")
+    ax[1].set_title("UMAP (embedded train data)")
+    ax[1].legend(handles=s.legend_elements()[0], labels=class_names_all)
+    s = ax[2].scatter(df_projection_unseen[:,0], df_projection_unseen[:,1], c=df_unseen.label_idx, s=5)
+    ax[2].set_aspect("equal", "datalim")
+    ax[2].set_title("UMAP (embedded unseen classes)")
+    ax[2].legend(handles=s.legend_elements()[0], labels=class_names_all)
+    fig.tight_layout()
+    plt.savefig("umap_embeddings.png")
