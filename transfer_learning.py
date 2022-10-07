ac02df7c826e135d4ce5ff0ba3897eabe2bc34ff
odgar2833 <odgar2833@birget.uib.no>
2022-10-07 17:26:03 +0200

    g2o

--- transfer_learning.py
+++ transfer_learning.py
@@ -24,9 +24,10 @@ def save_accuracy_plot(accuracies, n_samples, method):
     plt.ylabel("Test accuracy")
     plt.xticks(xi, n_samples, rotation=90)
     plt.title(method)
-    #plt.legend()
     plt.tight_layout()
-    plt.savefig("accuracy_" + method + ".png")
+    filename = "accuracy_" + method + ".png"
+    plt.savefig(filename)
+    print(f"Saved plot to {filename}.")
 
 if __name__ == "__main__":
     if not isfile("./embeddings_unseen.pkl"):
@@ -34,29 +35,30 @@ if __name__ == "__main__":
     
     # Load saved embeddings of the data with unseen classes and split
     df = pd.read_pickle("embeddings_unseen.pkl")
-    train, test = train_test_split(df, test_size=0.3, shuffle=True, random_state=0)
-    
+    train_embeddings, test_embeddings = train_test_split(df, test_size=0.3, shuffle=True, random_state=0)
+
     svm_classifier = make_pipeline(StandardScaler(), SVC(gamma="auto"))
     linear_classifier = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
-    k = 5
+    k = 10
     knn_classifier = KNeighborsClassifier(n_neighbors=k)
 
     n_samples = []
     svc_accuracies = []
     linear_accuracies = []
     knn_accuracies = []
-    sample_range = chain(range(10, 100, 10), range(100, 1000, 100), range(1000, len(train), 200))
+
+    sample_range = chain(range(10, 100, 10), range(100, 1000, 100), range(1000, len(train_embeddings), 200))
     for n in tqdm(list(sample_range)):
-        X_train = train.iloc[:n, 2:]
-        y_train = train.loc[:, "label_idx"].iloc[:n]
         # Fit models
+        X_train = train_embeddings.iloc[:n, 2:]
+        y_train = train_embeddings.loc[:, "label_idx"].iloc[:n]
         svm_classifier.fit(X_train, y_train)
         linear_classifier.fit(X_train, y_train)
         knn_classifier.fit(X_train, y_train)
 
         # Predict
-        X_test = test.iloc[:, 2:]
-        y_test = test.loc[:, "label_idx"]
+        X_test = test_embeddings.iloc[:, 2:]
+        y_test = test_embeddings.loc[:, "label_idx"]
         svc_preds = svm_classifier.predict(X_test)
         linear_preds = linear_classifier.predict(X_test)
         knn_preds = knn_classifier.predict(X_test)
@@ -71,22 +73,6 @@ if __name__ == "__main__":
         knn_accuracies.append(knn_accuracy)
         n_samples.append(n)
 
-    save_accuracy_plot(svc_accuracies, n_samples, "Support Vector Classifier")
-    save_accuracy_plot(linear_accuracies, n_samples, "Linear Classifier")
-    save_accuracy_plot(knn_accuracies, n_samples, f"k-Nearest Neighbors (k={k})")
-
-    """ 
-    dataset = FlowCamDataSet(class_names_unseen, image_size)
-
-    #Split into train and test data
-    val_size = int(0.1 * len(dataset))
-    test_size = int(0.3 * len(dataset))
-    train_size = len(dataset) - val_size - test_size
-    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(420))
-    for n in range(100, len(train_dataset), 200):
-        print(f"Training on {n} samples.")
-
-    #train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    #val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-    #test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    """
+    save_accuracy_plot(svc_accuracies, n_samples, "SVC")
+    save_accuracy_plot(linear_accuracies, n_samples, "Linear")
+    save_accuracy_plot(knn_accuracies, n_samples, "kNN")
